124aea9ea3ccd1a4f685a7130fd7c76e4c79e1bc
Lawrence Forooghian <lawrence@forooghian.com>
2022-07-06 10:50:23 -0300

    Add canonical docstring for Rest#push
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit 16d03a5.

--- ably.d.ts
+++ ably.d.ts
@@ -1766,6 +1766,10 @@ declare namespace Types {
      */
     time(callback?: Types.timeCallback): void;
     /**
+     * BEGIN CANONICAL DOCSTRING
+     * A [`Push`]{@link} object.
+     * END CANONICAL DOCSTRING
+     *
      * BEGIN LEGACY DOCSTRING
      * A reference to the `Push` object.
      * END LEGACY DOCSTRING
@@ -1836,6 +1840,10 @@ declare namespace Types {
      */
     time(): Promise<number>;
     /**
+     * BEGIN CANONICAL DOCSTRING
+     * A [`Push`]{@link} object.
+     * END CANONICAL DOCSTRING
+     *
      * BEGIN LEGACY DOCSTRING
      * A reference to the `Push` object.
      * END LEGACY DOCSTRING
